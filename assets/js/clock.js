b066505c2b9374b85afcaf42acae9866eb657e0f
Olena Korol <96837682+olenkor@users.noreply.github.com>
2023-01-19 16:48:21 +0000

    Sample code

---
+++ assets/js/clock.js
@@ -0,0 +1,11 @@
+(() => {
+    function updateClock() {
+        const clockContainer = document.querySelector('.clock');
+        clockContainer.innerText = new Date().toLocaleTimeString('uk');
+    }
+    setInterval(updateClock, 1000);
+})();
+
+
+
+
